1c9c28bba2fff11f1484ffebf25c8b4340a41931
Syed Bilal Ali <s.bilal@TechArabiya.local>
2020-01-08 17:40:02 +0500

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Navbar.scss";
+import logo from "../../logo.png";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import About from '../Tour/About';
+import Home from '../Tour/Home';
+import TourList from '../TourList/index'
+
+
+export default function Navbar() {
+    return (
+        <Router>
+        
+                <nav className="navbar">
+                <img src={logo} alt="city tours logo"/>
+                <ul className="nav-links">
+                    <li>
+                        <Link to="/" className="nav-link">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/About" className="nav-link">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/TourList" className="nav-link">
+                            Tours
+                        </Link>
+                    </li>
+
+                </ul>
+            </nav>
+                
+                    <Switch>
+                        <Route path="/About" component={About} />
+                        <Route exact path="/" component={Home} />
+                        <Route path="/TourList" component={TourList} />
+                    </Switch>
+                </Router>
+           
+       
+        
+   )
+}
